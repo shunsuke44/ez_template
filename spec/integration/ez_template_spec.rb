99658e32188b5f9453c8d313d252b381e05f3c24
Shunsuke Shimura <mt.cat.yakumo54@gmail.com>
2025-11-30 11:40:56 +0900

    test: Use unnamed class for template

--- spec/integration/ez_template_spec.rb
+++ spec/integration/ez_template_spec.rb
@@ -2,28 +2,28 @@
 
 require "spec_helper"
 
-module TestIntegration
-  class SampleTemplate < EzTemplate::Base
-    variable :animal do
-      case params[:animal].to_sym
-      when :dog
-        "a young dog"
-      when :cat
-        "an old cat"
+module EzTemplate
+  RSpec.describe EzTemplate do
+    let :template_class do
+      Class.new(EzTemplate::Base) do
+        variable :animal do
+          case params[:animal].to_sym
+          when :dog
+            "a young dog"
+          when :cat
+            "an old cat"
+          end
+        end
+
+        variable :place
+
+        variable(%r{https?://\w+\.com[/.\w]*$}) do |md|
+          uri = URI.parse(md[0])
+          "#{uri.scheme}://#{uri.host}/index.html"
+        end
       end
     end
 
-    variable :place
-
-    variable(%r{https?://\w+\.com[/.\w]*$}) do |md|
-      uri = URI.parse(md[0])
-      "#{uri.scheme}://#{uri.host}/index.html"
-    end
-  end
-end
-
-module EzTemplate
-  RSpec.describe EzTemplate do
     describe "#render" do
       let(:str) do
         <<~TEMPLATE
@@ -33,9 +33,7 @@ module EzTemplate
         TEMPLATE
       end
 
-      let :template do
-        ::TestIntegration::SampleTemplate.new
-      end
+      let(:template) { template_class.new }
 
       it "renders template with predefined variables" do
         r = template.parse(str)
@@ -56,10 +54,6 @@ module EzTemplate
 
           Please visit {{ http://foo.com/hogehuga/bar.html }}
         TEMPLATE
-      end
-
-      let :template_class do
-        ::TestIntegration::SampleTemplate
       end
 
       it "renders template with predefined variables" do
